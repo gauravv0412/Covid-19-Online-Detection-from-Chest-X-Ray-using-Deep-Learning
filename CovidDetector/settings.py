e810d77fb0cb17787242c540ab3896f36b9232d7
Gaurav Verma <gaurav.gv412@gmail.com>
2020-05-09 16:52:07 +0530

    Procfile

--- CovidDetector/settings.py
+++ CovidDetector/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0na^^mp6hyo6_2@%080r1=s_x*_9h$g1wiicmi)!r$a@r3@_+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
